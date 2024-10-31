5e4664ef8f83fba1b363c3bcbe40bab18aa55882
VirileMAI <fylhtqbhjcnbr@yandex.ru>
2024-10-31 23:58:05 +0300

    feat(main): add check for price, author name, year
    feat: add is_digits, is_not_alpha
    
    Добавил проверки на некорректные значения price и year и на отсутсвие цифр в имени автора
    
    Добавил функции is_digit и is_not_alpha для проверок

--- Bookstore.cpp
+++ Bookstore.cpp
@@ -1,40 +1,44 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <vector>
 #include <cctype>
+#include <limits>
 
 using namespace std;
 
-bool is_digits(string& str)
-{
+bool is_digits(const string& str) {
     for (char ch : str) {
-        int v = ch;
-        if (!isdigit(ch)) {
+        if (!isdigit(ch))
             return false;
-        }
     }
- 
     return true;
+}
+
+bool is_not_alphas(const std::string& str) {
+    for (char ch : str) {
+        if (isdigit(ch))
+            return true;
+    }
+    return false; 
 }
 
 enum class SortType { TITLE, AUTHOR, YEAR };
 
 class Book {
-  private:
+private:
     size_t _id;
     string _title;
     string _author;
     short _year;
     double _price;
 
-  public:
-    Book(size_t id, const string& title, const string& author, short year,
-         double price)
-        : _id(id), _title(title), _author(author), _year(year), _price(price){};
+public:
+    Book(size_t id, const string& title, const string& author, short year, double price)
+        : _id(id), _title(title), _author(author), _year(year), _price(price) {}
 
-    //геттеры
-    size_t GetID() { return _id; }
+    // Геттеры
+    size_t GetID() const { return _id; }
     const string& GetTitle() const { return _title; }
     const string& GetAuthor() const { return _author; }
     int GetYear() const { return _year; }
@@ -47,15 +51,14 @@ class Book {
     }
 
     bool operator<(const Book& other) const { return _title < other._title; }
-
     bool operator>(const Book& other) const { return _title > other._title; }
-
     bool operator==(const Book& other) const {
         return _title == other._title && _author == other._author &&
                _year == other._year && _price == other._price;
     }
 };
 
+// Функции для сортировки по автору и году
 bool compareByAuthor(const Book& a, const Book& b) {
     return a.GetAuthor() < b.GetAuthor();
 }
@@ -64,34 +67,36 @@ bool compareByYear(const Book& a, const Book& b) {
     return a.GetYear() < b.GetYear();
 }
 
-
-
 class BookStore {
-  private:
+private:
     vector<Book> _books;
     static size_t ID;
 
-  public:
-
+public:
     BookStore() { ID = 1; }
-    
-    void addBook(const Book& book) { 
-        _books.push_back(Book(ID, book.GetAuthor(), book.GetTitle(), book.GetYear(), book.GetPrice())); 
+
+    void addBook(const Book& book) {
+        _books.push_back(Book(ID, book.GetTitle(), book.GetAuthor(), book.GetYear(), book.GetPrice()));
         ID++;
     }
 
-    void removeBook(const string& title) {
-        for (auto it = _books.begin(); it < _books.end();) {
+    bool removeBook(const string& title) {
+        for (auto it = _books.begin(); it != _books.end();) {
             if (it->GetTitle() == title) {
                 _books.erase(it);
-                return;
-            } else it++;
+                cout << "Книга удалена.\n";
+                return true;
+            } else {
+                ++it;
+            }
         }
+        cout << "Книга с таким названием не найдена.\n";
+        return false;
     }
 
     Book* findBook(const string& title) {
-        for (auto it = _books.begin(); it < _books.end(); it++)
-            if (it->GetTitle() == title) return &(*it);
+        for (auto& book : _books)
+            if (book.GetTitle() == title) return &book;
         return nullptr;
     }
 
@@ -102,21 +107,22 @@ class BookStore {
                 break;
             case SortType::AUTHOR:
                 sort(_books.begin(), _books.end(), compareByAuthor);
+                break;
             case SortType::YEAR:
                 sort(_books.begin(), _books.end(), compareByYear);
+                break;
             default:
                 cout << "Неподходящий параметр.\n";
-                break;
+                return;
         }
-
         for (const auto& book : _books) book.Print();
     }
 
     vector<Book> findBookInRangePrice(double minPrice, double maxPrice) {
         vector<Book> result;
-        for (auto it = _books.begin(); it < _books.end(); it++) {
-            if (it->GetPrice() >= minPrice && it->GetPrice() <= maxPrice) {
-                result.push_back(*it);
+        for (const auto& book : _books) {
+            if (book.GetPrice() >= minPrice && book.GetPrice() <= maxPrice) {
+                result.push_back(book);
             }
         }
         return result;
@@ -129,8 +135,7 @@ void Menu() {
     cout << "\n1. Добавить книгу\n"
          << "2. Удалить книгу.\n"
          << "3. Найти книгу по названию.\n"
-         << "4. Показать все книги (Отсортированные по "
-            "названию/автору/году издания)\n"
+         << "4. Показать все книги (Отсортированные по названию/автору/году издания)\n"
          << "5. Найти все книги в ценовом диапазоне.\n"
          << "6. Выйти.\n"
          << "Выберите опцию: ";
@@ -138,54 +143,66 @@ void Menu() {
 
 int main() {
     BookStore store;
-    string tmp;
+    string tmpChoice;
     int choice;
 
     while (true) {
         Menu();
-        getline(cin, tmp);
+        getline(cin, tmpChoice);
 
-        if (tmp.empty()) 
-        {
-            continue;
-        }
-        if (!is_digits(tmp))
-        {
+        if (tmpChoice.empty()) continue;
+        if (!is_digits(tmpChoice)) {
             cout << "Неправильный ввод. Попробуйте снова.\n";
             continue;
         }
-        
-        choice = atoi(tmp.c_str());
-        string title, author;
+
+        choice = stoi(tmpChoice);
+        string title, author, tmpYear, tmpPrice;
         int year;
-        double price;
+        double price;   
 
         switch (choice) {
             case 1: {
                 cout << "Введите название: ";
-                cin.ignore(1, '\n');
                 getline(cin, title);
+
                 cout << "Введите автора: ";
                 getline(cin, author);
+
+                if (is_not_alphas(author)) {
+                    cout << "Некорректное имя автора, могут быть только буквы.\n";
+                    break;
+                }
+
                 cout << "Введите год издания: ";
-                cin >> year;
+                getline(cin, tmpYear);
+                if (!is_digits(tmpYear) || stoi(tmpYear) < 1 || stoi(tmpYear) > 2024)
+                {
+                    cout << "Некорректный ввод даты.\n";
+                    break;
+                }
+                year = stoi(tmpYear);
+
                 cout << "Введите цену: ";
-                cin >> price;
+                getline(cin, tmpPrice);
+                if (!is_digits(tmpPrice) || stoi(tmpPrice) < 0 || stoi(tmpPrice) > numeric_limits<int>::max())
+                {
+                    cout << "Некорректный ввод даты.\n";
+                    break;
+                }
+                price = stoi(tmpPrice);
                 store.addBook(Book(0, title, author, year, price));
                 cout << "Книга добавлена.\n";
             } break;
 
             case 2: {
                 cout << "Введите название книги для удаления: ";
-                cin.ignore(1, '\n');
                 getline(cin, title);
                 store.removeBook(title);
-                cout << "Книга удалена.\n";
             } break;
 
             case 3: {
                 cout << "Введите название книги для поиска: ";
-                cin.ignore(1, '\n');
                 getline(cin, title);
                 Book* book = store.findBook(title);
                 if (book) {
@@ -197,21 +214,36 @@ int main() {
 
             case 4: {
                 int sortOption;
-                cout << "Сортировать по: 1. Названию, 2. Автору, 3. Году "
-                        "издания\n";
+                cout << "Сортировать по: 1. Названию, 2. Автору, 3. Году издания\n";
                 cin >> sortOption;
-                store.listBooks((SortType)(sortOption - 1));
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                store.listBooks(static_cast<SortType>(sortOption - 1));
             } break;
 
             case 5: {
+                string tmpMin, tmpMax;
                 double minPrice, maxPrice;
                 cout << "Введите минимальную цену: ";
-                cin >> minPrice;
-                cout << "ВВедите максимальную цену: ";
-                cin >> maxPrice;
+                getline(cin, tmpMin);
+                if (!is_digits(tmpMin) || stoi(tmpMin) < 0 || stoi(tmpMin) > numeric_limits<int>::max())
+                {
+                    cout << "Введенна некоректная цена.\n";
+                    break;
+                }
+                minPrice = stoi(tmpMin);
+                cout << "Введите максимальную цену: ";
+                getline(cin, tmpMax);
+                if (!is_digits(tmpMax) || stoi(tmpMax) < 0 || stoi(tmpMax) > numeric_limits<int>::max())
+                {
+                    cout << "Введенна некоректная цена.\n";
+                    break;
+                }
+                maxPrice = stoi(tmpMax);
                 auto books = store.findBookInRangePrice(minPrice, maxPrice);
+                if (books.empty()) cout << "Нет книг в таком ценовом диапазоне.\n";
                 for (const auto& book : books) book.Print();
             } break;
+
             case 6:
                 cout << "Выход из программы.\n";
                 return 0;
